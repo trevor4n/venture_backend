22552af73dd2d1fad09a3cfdd0be12ec58abe1a6
trevor4n <trevor4n@gmail.com>
2021-09-13 21:31:14 -0500

    initial migration, admin console, django extensions (shell_plus --ipython)

--- venture/admin.py
+++ venture/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Trip, Guideline, Restriction, Airline
+
+admin.site.register(Trip)
+admin.site.register(Guideline)
+admin.site.register(Restriction)
+admin.site.register(Airline)
